efe3740203abb55ca3fa73315cc2f79d64850d3f
ryan <8278@shucom.local>
2021-06-18 14:02:20 +0800

    [UPDATE] finish hello world：

---
+++ helloworld/hello_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+    assertCorrectMessage:= func(t *testing.T, got, want string) {
+        //声明辅助函数
+        t.Helper()
+        if got != want {
+            t.Errorf("got '%q' want '%q'", got, want)
+        }
+    }
+
+    t.Run("saying hello to people", func(t *testing.T) {
+        got := Hello("Crist")
+        want := "Hello Crist"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("saying hello world when empty", func(t *testing.T) {
+        got := Hello("")
+        want := "Hello World"
+        assertCorrectMessage(t, got, want)
+    })
+
+}
